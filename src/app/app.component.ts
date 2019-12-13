0e7e63efbca39e04031b5d6729a6d5a6abd2a899
Shavonne Cobb <Shavonne.Cobb@improving.com>
2019-12-13 08:04:56 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: []
 })
 export class AppComponent {
   title = 'millennial';
+
+  display: boolean = false;
+  displayNav: boolean = false;
+  buttonDisplay: boolean = true;
+
+  showNav() {
+    this.displayNav = true;
+  }
+
+  showMore() {
+    this.display = true;
+    this.buttonDisplay = false;
+  }
 }
---
+++ src/app/open-close.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
+
+@Component({
+  selector: 'app-open-close',
+  animations: [
+    trigger('openClose', [
+      // ...
+      state('open', style({
+        height: '200px',
+        opacity: 1,
+        backgroundColor: 'yellow'
+      })),
+      state('closed', style({
+        height: '100px',
+        opacity: 0.5,
+        backgroundColor: 'green'
+      })),
+      transition('open => closed', [
+        animate('1s')
+      ]),
+      transition('closed => open', [
+        animate('0.5s')
+      ]),
+    ]),
+  ],
+  templateUrl: 'open-close.component.html',
+  styleUrls: ['open-close.component.css']
+})
+export class OpenCloseComponent {
+  isOpen = true;
+
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
+
+}
